9458eb43633d77e09d94953493ba45002fd6751a
João Henriques <joaoh88@gmail.com>
2023-01-04 21:55:08 +0000

    Added logic and example service

--- dell_fan_monitor.sh
+++ dell_fan_monitor.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-TEMPERATURE_FILE=/sys/class/hwmon/hwmon2/temp1_input
+TEMPERATURE_FILE=/sys/class/hwmon/hwmon1/temp1_input
 CONFIG_FILE=/opt/dell-fan-monitor/speeds.conf
 DELLFAN=/opt/dellfan/dellfan
 INIT_SPD=3
 
 # State variables
 declare -A speeds
-total_speeds=0
+max_state=0
 CUR_ST=-1
 EXIT=0
+NUM_MASK="^[-]?[0-9]+$"
+: "${SLEEP_TIME:=3}"
 
 perror() {
     echo "[ERROR] ${@}" >&2
@@ -49,14 +51,21 @@ read_config() {
     speeds=()
     while IFS=' ' read -r sp low high; do
         ( [ "x$sp" == "x" ] || [ "x$low" == "x" ] || [ "x$high" == "x" ] ) && continue
+        if ! [[ $sp =~ $NUM_MASK ]] || ! [[ $low =~ $NUM_MASK ]] || ! [[ $high =~ $NUM_MASK ]]; then
+            perror "Values '$sp', '$low' and '$high' must all be valid integers! Exiting..."
+            exit 1
+        fi
+        low=$(($low * 1000))
+        high=$(($high * 1000))
 
         #pinfo "[$total_speeds] Adding '$sp' '$low' '$high'"
+
         speeds["${total_speeds}.s"]="$sp"
         speeds["${total_speeds}.l"]="$low"
         speeds["${total_speeds}.h"]="$high"
         total_speeds=$((total_speeds+1))
 
-        if [ "x$INIT_SPD" == "x$sp" ]; then
+        if [ "$INIT_SPD" == "$sp" ]; then
             CUR_ST=$total_speeds
             pinfo "Found initial speed '$INIT_SPD' in state '$total_speeds'"
         fi
@@ -67,7 +76,7 @@ read_config() {
         perror "Speeds $total_speeds not found"
         exit 1
     fi
-
+    max_state=$((total_speeds-1))
     if [ "$CUR_ST" == "-1" ]; then
         CUR_ST=0
         INIT_SPD=${speeds["$CUR_ST.s"]}
@@ -109,9 +118,47 @@ fi
 
 read_config
 
+
+################################################################
+# Start looping
+################################################################
+
+sleep 2
 #pinfo "Setting initial speed of '$INIT_SPD'"
 set_speed "$INIT_SPD"
 
+while true; do
+    sleep "$SLEEP_TIME"
+    OLD_ST="$CUR_ST"
 
-pinfo $(read_temp)
+    c_low="${speeds["${CUR_ST}.l"]}"
+    c_high="${speeds["${CUR_ST}.h"]}"
 
+    temp="$(read_temp)"
+
+    if [[ $temp -lt $c_low ]]; then
+        CUR_ST=$((CUR_ST-1))
+
+        if [[ $CUR_ST -lt 0 ]]; then
+            CUR_ST=0
+        fi
+
+    elif [[ $temp -gt $c_high ]]; then
+        CUR_ST=$((CUR_ST+1))
+
+        if [[ $CUR_ST -gt $max_state ]]; then
+            CUR_ST=$max_state
+        fi
+    fi
+
+    #pinfo "$temp : $c_low : $c_high : $OLD_ST : $CUR_ST"
+    if [ "$OLD_ST" != "$CUR_ST" ]; then
+        new_speed="${speeds["${CUR_ST}.s"]}"
+        pinfo "Temperature is '$temp', changing speed to new state $CUR_ST [${new_speed}]'"
+        if [ "$CUR_ST" == "$max_state" ]; then
+            pwarn "Temperature is hitting max state!"
+        fi
+        set_speed "$new_speed"
+    fi
+
+done
